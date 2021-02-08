a0dae7e50d3ccde244f22b743fee84885d7ce4ea
Jason <jkasari@outlook.com>
2021-02-07 22:27:16 -0800

    Uploading my progress
    I've been working on this code alot just on the arduino. It's currently
    at a point that I'm happy with so I'm uploading it.

--- main.ino
+++ main.ino
@@ -7,59 +7,56 @@ void setup() {
   strip.setBrightness(255);
   strip.clear();
   strip.show();
-  startUp();
+  startUp(8); //8 is soft white on the color code index.
   pinMode(button1, INPUT);
   pinMode(button2, INPUT);
 }
 
-
 int buttonState1 = digitalRead(button1);
 int buttonState2 = digitalRead(button2);
 int lightShow = 1;
-uint8_t brightnessStand = 255; //Brightness standard, each display shares/changes this specific number via |buttonCheck|.
+uint8_t brightnessStand = 200;
+          // Brightness standard, each display shares/changes this specific
+         // number via |buttonCheck|.
 bool on = true;
 
 void loop() {
   int buttonState1 = digitalRead(button1);
   int buttonState2 = digitalRead(button2);
 
-  
-  if(buttonState1 == HIGH && buttonState2 == HIGH) {
-    delay(1000);
-    if(buttonState1 == HIGH && buttonState2 == HIGH) {
-      lightShow += 1;
-      strip.clear();
-    }
+  if (!on) {
+    lightShow += 1;
+    strip.clear();
+    on = true;
   }
 
-  //All displays share at least two of the same variables: |on| |brightnessStand|.
-  //Each display relies on |on| to keep the loop going. They also take |brightnessStand|
-  //only for the purpose of changing it with |buttonCheck|. |brightnessStand| is never 
-  //manipulated or used in actual logic of a display.
-  
-  switch(lightShow) {
-   case 1:
-    lightAll(on, brightnessStand);
-    on = true;
+  // All displays share at least two of the same variables: |on|
+  // |brightnessStand|. Each display relies on |on| to keep the loop going. They
+  // also take |brightnessStand| only for the purpose of changing it with
+  // |buttonCheck|. |brightnessStand| is never manipulated or used in actual
+  // logic of a display.
+
+  switch (lightShow) {
+  case 1:
+    lightAll(on, brightnessStand, 8);
     break;
-   case 2:
-    //Number of leds to randomly flicker, not the size of the area you want them in. 
-    //The last variable is the speed of the flickering. 
-    randColorFlicker(on, brightnessStand, LED_COUNT, 0); 
-    on = true;
+  case 2:
+    // Number of leds to randomly flicker, not the size of the area you want
+    // them in.
+    randColorFlicker(on, brightnessStand, LED_COUNT);
     break;
-   case 3:
-    flame(on, brightnessStand, LED_COUNT, 30);
-    on = true;
+  case 3:
+    flame(on, brightnessStand, LED_COUNT);
     break;
-   case 4:
-    recursion(on, brightnessStand, 0);
-    on = true;
+  case 4:
+    lavaLamp(on, brightnessStand);
     break;
-   case 5:
-    blob(on, brightnessStand, LED_COUNT);
-    on = true;
+  case 5:
+    randColorFade(on, brightnessStand);
+    break;
+  case 6:
+    stars(on, brightnessStand);
     lightShow = 0;
     break;
-   }
+  }
 }
